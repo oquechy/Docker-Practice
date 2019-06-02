da165af0d75927b588920dba41be68ead0c42738
oquechy <vasilliza@gmail.com>
2019-06-03 00:43:45 +0300

    add deps

--- increment.py
+++ increment.py
@@ -3,15 +3,18 @@ import time
 
 import boto3
 
+time.sleep(20)
+
 if len(sys.argv) != 3:
     print(f'Expected 2 parameters with queue names, got {len(sys.argv) - 1}')
     exit(1)
 
-sqs = boto3.resource('sqs', endpoint_url=f'http://localhost:4576/')
+sqs = boto3.resource('sqs', endpoint_url='http://localstack:4576/')
 read_queue = sqs.get_queue_by_name(QueueName=sys.argv[1])
 write_queue = sqs.get_queue_by_name(QueueName=sys.argv[2])
 
 while True:
+    print('.')
     for message in read_queue.receive_messages():
         number = message.body
         print(number)
